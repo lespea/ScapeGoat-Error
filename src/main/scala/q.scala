f3e64f7320e3c8ab0163fdf40769e14b10dea374
Adam Lesperance <alesperance@carlson.com>
2014-08-29 11:05:18 -0500

    Test final case class differences

--- src/main/scala/q.scala
+++ src/main/scala/q.scala
@@ -1,5 +1,21 @@
-case class CombinedInfo[A](
-  combined: A,
-  extraIPs: Set[Long],
-  extraHosts: Set[String],
-  notes: Set[String])
+case class TestClass(attr: Int, var varAttr: Int, private val privateValAttr: Int, private var privateVarAttr: Int) {
+  val innerVal = 5
+  final val innerValFinal = 5
+  def defTest = 5
+  final def TestFinal = 5
+}
+
+final case class TestFinalClass(attr: Int, var varAttr: Int, private val privateValAttr: Int, private var privateVarAttr: Int) {
+  val innerVal = 5
+  final val innerValFinal = 5
+  def defTest = 5
+  final def TestFinal = 5
+}
+
+object Test {
+  val a = TestClass(1, 1, 1, 1)
+  val b = TestFinalClass(1, 1, 1, 1)
+
+  final val finalA = TestClass(1, 1, 1, 1)
+  final val finalB = TestFinalClass(1, 1, 1, 1)
+}
